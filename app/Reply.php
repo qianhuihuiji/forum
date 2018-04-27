92a1f18e372b9fcd93a503c2573a113089a885c4
meiyiming <meiyiminghuiji@gmail.com>
2018-04-27 08:43:29 +0000

    chapter 3 end

--- app/Reply.php
+++ app/Reply.php
@@ -24,4 +24,9 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     protected $guarded = [];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class,'user_id');  // 使用 user_id 字段进行模型关联
+    }
 }
