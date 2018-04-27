aece7721ade545d852e1a36aa68a029e3a341abb
meiyiming <meiyiminghuiji@gmail.com>
2018-04-27 03:19:38 +0000

    Chepter 1

--- app/Thread.php
+++ app/Thread.php
@@ -4,6 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Thread
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $body
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Thread whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Thread extends Model
 {
     protected $guarded = [];
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -32,3 +32,15 @@ $factory->define(App\Thread::class,function ($faker){
        'body' => $faker->paragraph,
     ];
 });
+
+$factory->define(App\Reply::class,function ($faker){
+    return [
+        'thread_id' => function () {
+            return factory('App\Thread')->create()->id;
+        },
+        'user_id' => function () {
+            return factory('App\User')->create()->id;
+        },
+        'body' => $faker->paragraph,
+    ];
+});
