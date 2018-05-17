9daca2dd9675662ab25e3a3a1fd57cf26652935b
meiyiming <meiyiminghuiji@gmail.com>
2018-05-17 09:45:27 +0000

    chapter 25 finish

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -83,6 +83,8 @@ class ThreadsController extends Controller
 
     public function destroy($channel,Thread $thread)
     {
+        $this->authorize('update',$thread);
+
         $thread->delete();
 
         if(request()->wantsJson()){
