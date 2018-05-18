acbe962e85ccdbce090a24b78085b0fed8f61187
meiyiming <meiyiminghuiji@gmail.com>
2018-05-18 02:05:12 +0000

    chapter 25 finish

--- app/Thread.php
+++ app/Thread.php
@@ -24,6 +24,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Thread extends Model
 {
+    use RecordsActivity;
+
     protected $guarded = [];
     protected $with = ['creator','channel'];
 
@@ -37,7 +39,7 @@ class Thread extends Model
 
         static::deleting(function ($thread) {
             $thread->replies()->delete();
-    });
+        });
     }
 
     public function path()
