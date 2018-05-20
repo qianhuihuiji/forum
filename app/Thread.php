13759ef8428ca154d7dac0d43826b8deb5f3ffba
meiyiming <meiyiminghuiji@gmail.com>
2018-05-20 12:18:14 +0000

    chapter 29 finish

--- app/Thread.php
+++ app/Thread.php
@@ -38,7 +38,7 @@ class Thread extends Model
         });
 
         static::deleting(function ($thread) {
-            $thread->replies()->delete();
+            $thread->replies->each->delete();
         });
     }
 
