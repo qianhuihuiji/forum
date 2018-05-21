67e0c417c52d41406d342fcd3bc29c150b63c444
meiyiming <meiyiminghuiji@gmail.com>
2018-05-21 14:00:25 +0000

    chapter 29 finish'

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,6 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +14,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('flash', require('./components/Flash.vue'));
 
 const app = new Vue({
     el: '#app'
