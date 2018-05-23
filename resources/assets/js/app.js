148529946266d3e8b7b2640323010942396be1df
meiyiming <meiyiminghuiji@gmail.com>
2018-05-23 07:29:27 +0000

    chapter 33 finish

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,6 +15,7 @@ require('./bootstrap');
  */
 
 Vue.component('flash', require('./components/Flash.vue'));
+Vue.component('reply', require('./components/Reply.vue'));
 
 const app = new Vue({
     el: '#app'
