81627990d2a1ed6d0a5105a2b8d266a07f442d2c
meiyiming <meiyiminghuiji@gmail.com>
2018-06-21 09:35:58 +0000

    chapter 67 finish

--- app/Trending.php
+++ app/Trending.php
@@ -8,14 +8,24 @@ class Trending
 {
     public function get()
     {
-        return array_map('json_decode',Redis::zrevrange('trending_threads',0,4));
+        return array_map('json_decode',Redis::zrevrange($this->cacheKey(),0,4));
     }
 
     public function push($thread)
     {
-        Redis::zincrby('trending_threads',1,json_encode([
+        Redis::zincrby($this->cacheKey(),1,json_encode([
             'title' => $thread->title,
             'path' => $thread->path()
         ]));
+    }
+
+    public function cacheKey()
+    {
+        return app()->environment('testing') ? 'testing_trending_threads' : 'trending_threads';
+    }
+
+    public function reset()
+    {
+        Redis::del($this->cacheKey());
     }
 }
