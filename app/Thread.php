586c28b0c5179dc14237295dd9ca12faf5e86164
meiyiming <meiyiminghuiji@gmail.com>
2018-06-22 13:26:58 +0000

    chapter 70 finish

--- app/Thread.php
+++ app/Thread.php
@@ -110,8 +110,8 @@ class Thread extends Model
         return $this->updated_at > cache($key);
     }
 
-    public function visits()
-    {
-        return new Visits($this);
-    }
+//    public function visits()
+//    {
+//        return new Visits($this);
+//    }
 }
